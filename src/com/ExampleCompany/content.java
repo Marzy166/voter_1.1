6774f2c3218b83076fb67eb4fad3e46443d6794d
MAL09150555 <MAL09150555@hull-college.ac.uk>
2016-01-12 14:21:40 +0000

    Added a new window/button for a login page. Buttons will be added for a login and then will connect to a server afterwards

--- src/com/ExampleCompany/content.java
+++ src/com/ExampleCompany/content.java
@@ -2,6 +2,8 @@ package com.ExampleCompany;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import javax.swing.JRadioButton;
 
 /**
  * Created by mar08139317 on 04/01/2016.
@@ -12,16 +14,23 @@ public class content {
         JPanel content = new JPanel();
         content.setPreferredSize(new Dimension(400, 400));
         content.setBackground(Color.white);
-        JLabel home_text = new JLabel("Our online Ballot Box provides quick and easy voting for any user");
-        JLabel home_ln2 = new JLabel("also make sure that the security of each user voter is kept safe.");
-        JLabel home_ln3 = new JLabel("With most transactions taking place online, it is useful to be");
-        JLabel home_ln4 = new JLabel("able to vote online too");
-        JLabel home_ln5 = new JLabel("It's faster, easier and more convenient.");
+        JLabel home_text = new JLabel("Welcome to your schools electronic ballot box! Please find");
+        JLabel home_ln2 = new JLabel("all the necessary information in the other options in the app.");
+        JLabel home_ln3 = new JLabel("Remember, every vote counts!");
         content.add(home_text);
         content.add(home_ln2);
         content.add(home_ln3);
-        content.add(home_ln4);
-        content.add(home_ln5);
+        return content;
+    }
+
+    public JPanel login(){
+        JPanel content = new JPanel();
+
+        content.setPreferredSize(new Dimension(400, 400));
+        content.setBackground(Color.white);
+        JLabel home_text = new JLabel("Please Login below:");
+        content.add(home_text);
+
         return content;
     }
 
@@ -31,8 +40,30 @@ public class content {
 
         content.setPreferredSize(new Dimension(400, 400));
         content.setBackground(Color.white);
-        JLabel home_text = new JLabel("This is a vote box");
+        JLabel home_text = new JLabel("Please vote for the preferred student below:");
         content.add(home_text);
+
+        // Aidan - Added buttons below so they appear in a 2 x 2 grid with display of names next to each radio button.
+
+        JRadioButton button1 = new JRadioButton("John Smith");
+        JRadioButton button2 = new JRadioButton("Mary Pane");
+        JRadioButton button3 = new JRadioButton("Jane Doe");
+        JRadioButton button4 = new JRadioButton("Jack Crack");
+
+        ButtonGroup buttongroup = new ButtonGroup();
+        buttongroup.add(button1);
+        buttongroup.add(button2);
+        buttongroup.add(button3);
+        buttongroup.add(button4);
+
+        JPanel radioPanel = new JPanel();
+        radioPanel.setLayout(new GridLayout(2,2));
+        radioPanel.add(button1);
+        radioPanel.add(button2);
+        radioPanel.add(button3);
+        radioPanel.add(button4);
+
+        content.add(radioPanel);
         return content;
     }
 
@@ -42,18 +73,16 @@ public class content {
 
         content.setPreferredSize(new Dimension(400, 400));
         content.setBackground(Color.white);
-        JLabel home_text = new JLabel("Not many people are a fan of paper ballots, the Administer ");
-        JLabel home_ln2 = new JLabel("has to create the ballots from scratch and manage the responses.");
-        JLabel home_ln3 = new JLabel("They also have to track every ballot cast without revealing");
-        JLabel home_ln4 = new JLabel("who cast them and manage the disposal of the ballots");
-        JLabel home_ln5 = new JLabel("They do this all while calculating results.");
-        JLabel home_ln6 = new JLabel("There is also the costs of printing the ballots and postage");
+        JLabel home_text = new JLabel("Voting is a fair system that show popularity and a margin");
+        JLabel home_ln2 = new JLabel("of which student will win the election for presidency");
+        JLabel home_ln3 = new JLabel("in 2016. Every student who participates will decide");
+        JLabel home_ln4 = new JLabel("who should win and the majority of the votes will");
+        JLabel home_ln5 = new JLabel("be counted at the end of the 3 month period.");
         content.add(home_text);
         content.add(home_ln2);
         content.add(home_ln3);
         content.add(home_ln4);
         content.add(home_ln5);
-        content.add(home_ln6);
         return content;
     }
 
@@ -63,12 +92,11 @@ public class content {
 
         content.setPreferredSize(new Dimension(400, 400));
         content.setBackground(Color.white);
-        JLabel home_text = new JLabel("For any help or advise, please contact us from our email addresses:");
-        JLabel home_ln2 = new JLabel("Aaron.mars@hotmail.co.uk");
-        JLabel home_ln3 = new JLabel("");
+        JLabel home_text = new JLabel("For any help or advice, please contact us from our email addresses:");
+        JLabel home_ln2 = new JLabel("aaron.mars@hotmail.co.uk or aidan-malone@hotmail.com");
         content.add(home_text);
         content.add(home_ln2);
-        content.add(home_ln3);
         return content;
     }
 }
+
--- src/com/ExampleCompany/jframe.java
+++ src/com/ExampleCompany/jframe.java
@@ -18,105 +18,124 @@ public class jframe{
     public static void main(String[] args) {
 
 
-            JFrame myFirstFrame = new JFrame("Voting Application");
-            myFirstFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            myFirstFrame.setSize(600, 400);
-            myFirstFrame.setVisible(true);
+        JFrame myFirstFrame = new JFrame("Voting Application");
+        myFirstFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        myFirstFrame.setSize(600, 400);
+        myFirstFrame.setVisible(true);
 
-            JPanel menu = new JPanel();
-            menu.setBackground(Color.decode("#80beff"));
-            menu.setAlignmentX(Component.LEFT_ALIGNMENT);
-            JPanel content = new JPanel();
-            JPanel in_content = new JPanel();
-            content.setAlignmentX(Component.LEFT_ALIGNMENT);
-            content.setSize(200, 200);
+        JPanel menu = new JPanel();
+        menu.setBackground(Color.decode("#80beff"));
+        menu.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JPanel content = new JPanel();
+        JPanel in_content = new JPanel();
+        content.setAlignmentX(Component.LEFT_ALIGNMENT);
+        content.setSize(200, 200);
 
-            content.add(in_content);
-            content.setBackground(Color.white);
-            JButton home =  new JButton("Home");
-            JButton vote =  new JButton("Vote");
-            JButton reason =  new JButton("Why Vote?");
-            JButton help = new JButton("Help/Contact");
+        content.add(in_content);
+        content.setBackground(Color.white);
 
-            menu.add(home);
-            menu.add(vote);
-            menu.add(reason);
-            menu.add(help);
-            //Aidan - Added taskbar and buttons for app.
-
-            BoxLayout boxlayout = new BoxLayout (myFirstFrame.getContentPane(), BoxLayout.Y_AXIS);
-            myFirstFrame.setLayout(boxlayout);
-
-            myFirstFrame.add(menu);
-            myFirstFrame.add(content);
-
-            // Run home as first
-            content new_panel = new content();
-            content.removeAll();
-            in_content.add(new_panel.home());
-            in_content.setBackground(Color.white);
-            content.add(in_content);
-            myFirstFrame.pack();
+        JButton home =  new JButton("Home");
+        JButton login = new JButton("Login");
+        JButton vote =  new JButton("Vote");
+        JButton reason =  new JButton("Why Vote?");
+        JButton help = new JButton("Help/Contact");
 
 
-                myFirstFrame.setVisible(true);
+        menu.add(home);
+        menu.add(login);
+        menu.add(vote);
+        menu.add(reason);
+        menu.add(help);
+        //Aidan - Added taskbar and buttons for app.
 
-            // home button event
-            home.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    content new_panel = new content();
-                    content.removeAll();
-                    JPanel in_content = new JPanel();
-                    in_content.setBackground(Color.white);
-                    in_content.add(new_panel.home());
-                    content.add(in_content);
-                    myFirstFrame.pack();
+        BoxLayout boxlayout = new BoxLayout (myFirstFrame.getContentPane(), BoxLayout.Y_AXIS);
+        myFirstFrame.setLayout(boxlayout);
 
-                }
-            });
+        myFirstFrame.add(menu);
+        myFirstFrame.add(content);
 
-            // vote button event
-            vote.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    content new_panel = new content();
-                    content.removeAll();
-                    JPanel in_content = new JPanel();
-                    in_content.setBackground(Color.white);
-                    in_content.add(new_panel.vote());
-                    content.add(in_content);
-                    myFirstFrame.pack();
+        // Run home as first
+        content new_panel = new content();
+        content.removeAll();
+        in_content.add(new_panel.home());
+        in_content.setBackground(Color.white);
+        content.add(in_content);
+        myFirstFrame.pack();
 
-                }
-            });
 
-            // reason button event
-            reason.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    content new_panel = new content();
-                    content.removeAll();
-                    JPanel in_content = new JPanel();
-                    in_content.setBackground(Color.white);
-                    in_content.add(new_panel.reason());
-                    content.add(in_content);
-                    myFirstFrame.pack();
+        myFirstFrame.setVisible(true);
 
-                }
-            });
+        // home button event
+        home.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                content new_panel = new content();
+                content.removeAll();
+                JPanel in_content = new JPanel();
+                in_content.setBackground(Color.white);
+                in_content.add(new_panel.home());
+                content.add(in_content);
+                myFirstFrame.pack();
 
-            // help button event
-            help.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    content new_panel = new content();
-                    content.removeAll();
-                    JPanel in_content = new JPanel();
-                    in_content.setBackground(Color.white);
-                    in_content.add(new_panel.help());
-                    content.add(in_content);
-                    myFirstFrame.pack();
+            }
+        });
 
-                }
-            });
-        }
+
+        login.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                content new_panel = new content();
+                content.removeAll();
+                JPanel in_content = new JPanel();
+                in_content.setBackground(Color.white);
+                in_content.add(new_panel.login());
+                content.add(in_content);
+                myFirstFrame.pack();
+
+            }
+        });
+
+        // vote button event
+        vote.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                content new_panel = new content();
+                content.removeAll();
+                JPanel in_content = new JPanel();
+                in_content.setBackground(Color.white);
+                in_content.add(new_panel.vote());
+                content.add(in_content);
+                myFirstFrame.pack();
+
+
+            }
+        });
+
+        // reason button event
+        reason.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                content new_panel = new content();
+                content.removeAll();
+                JPanel in_content = new JPanel();
+                in_content.setBackground(Color.white);
+                in_content.add(new_panel.reason());
+                content.add(in_content);
+                myFirstFrame.pack();
+
+            }
+        });
+
+        // help button event
+        help.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                content new_panel = new content();
+                content.removeAll();
+                JPanel in_content = new JPanel();
+                in_content.setBackground(Color.white);
+                in_content.add(new_panel.help());
+                content.add(in_content);
+                myFirstFrame.pack();
+
+            }
+        });
+    }
 
 
 }
